d3885f8dca78f02e6b9863440cd7fd21e977f8a3
Kristian <k.numme05@gmail.com>
2025-10-18 20:03:42 +0200

    product component is good

---
+++ whitewash/src/app/components/shopping-cart/Product.tsx
@@ -0,0 +1,21 @@
+
+import type { product } from "@/search/page"
+
+type ProductProps = {
+    product: product
+};
+
+export function Product(props: ProductProps) {
+    return (
+        <div className="flex flex-row justify-between p-5 py-3 min-w-100">
+            <div className="flex flex-col">
+                <span className="font-bold text-rema-secondary-darkblue text-xl">{props.product.name}</span>
+                <span className="">{props.product.description}</span>
+            </div>
+            <div className="flex flex-col text-right">
+                <span className="font-bold text-rema-secondary-blue text-lg ">{`${props.product.price} kr`}</span>
+                <span className="text-xs text-rema-secondary">{`${props.product.pricePerUnit} kr/${props.product.unit}`}</span>
+            </div>
+        </div>
+    )
+}
--- whitewash/src/app/search/page.tsx
+++ whitewash/src/app/search/page.tsx
@@ -1,16 +1,18 @@
 'use client'
 
 import { Header } from "@/components/header/header"
+import { Product } from "@/components/shopping-cart/Product";
 import { useSearchParams } from "next/navigation"
 import { useEffect, useState } from "react";
 
-type product = {
+export type product = {
     productid: number,
     gtin: string,
     name: string,
     description: string,
     price: number,
     pricePerUnit: number,
+    priceperunit: number,
     unit: string,
     allergens: string,
     carbonFootprintGram: number,
@@ -34,7 +36,7 @@ export default function SearchPage(){
                 const result = await response.json();
                 setData(result);
                 setLoading(false);
-                // console.log(result)
+                console.log(result)
             } catch (error) {
                 console.error("Error fetching data:", error);
             }
@@ -49,17 +51,14 @@ export default function SearchPage(){
             <Header />
             <div className="flex flex-col">
                 {data && data.length > 0 ? 
-                    (
-                    <ul>
-                        {data.map((vare: product) => (
-                        
-                        <li key={vare.productid}> 
-                            {/* Vare component */}
-                            {vare.name} 
-                        </li>
-                        ))}
-                    </ul>
-                    ) 
+                    data.map((vare: product) => (
+                    <div className="flex flex-row w-120 justify-between" key={vare.productid}>
+                        <Product product={vare}  />
+
+                        <button type="button">Legg til</button>
+                    </div>
+
+                    ))
                     : 
                     <div>
                         {loading ? "Loading..." : "Fant ingen resultater"}
