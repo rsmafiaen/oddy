1fed3d24e5281c14e12edbcce49bf018428e87e6
Nicolay B. Rennemo <rennemo@proton.me>
2025-10-19 01:46:20 +0200

    I hate linting oh my days

--- whitewash/src/app/page.tsx
+++ whitewash/src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useEffect, useState } from "react"
-import { Header } from "@/components/header/header"
+import { Header } from "@/components/header/Header"
 import { OddyChat } from "./components/chat/OddyChat"
 import { Oddy } from "./components/oddy/Oddy"
 import Popover from "@mui/material/Popover"
