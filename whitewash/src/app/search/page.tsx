9d152820513284698dab27be822d587950937203
Henry Græsberg <108506830+henrygraesberg@users.noreply.github.com>
2025-10-19 12:08:35 +0200

    Show Oddy instantly if search yields no results

--- whitewash/src/app/search/page.tsx
+++ whitewash/src/app/search/page.tsx
@@ -36,8 +36,8 @@ export default function SearchPage() {
 		},
 	})
 
-	const oddyMessage = `Jeg har søkt på ${search} på nettsiden din, og fått opp disse resultatene (i json format): ${JSON.stringify(data)}. Gi meg en anbefaling på hva jeg burde kjøpe basert på CO2 fotavtrykket til varene, pris og sunnhet. Til senere svar, vennligst bare referer til disse varene og baser svar på resultatene`
-	const { data: oddyResponse } = useQuery({
+	const oddyMessage = `Jeg har søkt på ${search} på nettsiden din, og fått opp disse resultatene (i json format): ${JSON.stringify(data)}. Gi meg en anbefaling på hva jeg burde kjøpe basert på CO2 fotavtrykket til varene, pris og sunnhet. Til senere svar, vennligst bare referer til disse varene og baser svar på resultatene. Du har ikke tilgang til å finne mer informasjon om varer på egen hånd. Om du trenger mer informasjon, be meg om å starte ett nytt søk etter det du leter etter`
+	const { data: oddyResponse, isLoading: oddyLoading } = useQuery({
 		queryKey: ["Oddy", "oddySearch", JSON.stringify(data)],
 		queryFn: async () => {
 			const url = "http://localhost:4000/api/oddy?inMessage="
@@ -46,7 +46,7 @@ export default function SearchPage() {
 
 			return (await res.json()) as { message: string }
 		},
-		enabled: !!data,
+		enabled: !!data && data.length > 0,
 	})
 
 	const handleToggleChat = () => setChattingWithOddy((v) => !v)
@@ -67,12 +67,34 @@ export default function SearchPage() {
 								content: oddyResponse.message,
 							},
 						]
-					: []
+					: [
+							{
+								id: "0",
+								role: "user",
+								content:
+									oddyMessage +
+									"Om det ikke er noen resultater, spør om det er noe annet du kan hjelpe med.",
+							},
+							{
+								id: crypto.randomUUID(),
+								role: "assistant",
+								content:
+									"Set ut som vi ikke fant noen resultater for søket ditt. Er det noe annet jeg kan hjelpe med?",
+							},
+						]
 			}
 		/>
 	) : (
-		oddyResponse && (
-			<Oddy message={oddyResponse.message} onClick={handleToggleChat} />
+		!oddyLoading &&
+		!!data && (
+			<Oddy
+				message={
+					data.length > 0 && oddyResponse
+						? oddyResponse.message
+						: "Set ut som vi ikke fant noen resultater for søket ditt. Er det noe annet jeg kan hjelpe med?"
+				}
+				onClick={handleToggleChat}
+			/>
 		)
 	)
 
